19337fe9032693fe81029ba8312c950c00cd8613
Thiago Trindade <thiago.trindadexd@gmail.com>
2023-06-29 11:02:06 -0300

    feat: create initial database

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,8 +1,7 @@
 import { adaptRoute } from '@/main/adapters'
 import { makeUserController } from '@/main/factories/controllers'
-import { auth } from '@/main/middlewares/auth'
 import { RequestHandler, Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/user', auth as RequestHandler, adaptRoute(makeUserController()) as RequestHandler)
+  router.post('/user', adaptRoute(makeUserController()) as RequestHandler)
 }
