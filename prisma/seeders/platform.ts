780d2e24ee0bcbffddadf82f6f8736339ef9c0e0
Thiago Trindade <thiago.trindadexd@gmail.com>
2023-06-29 15:45:47 -0300

    feat: create seed to populate database

---
+++ prisma/seeders/platform.ts
@@ -0,0 +1,34 @@
+export const platform = [
+  {
+    id: 1,
+    description: "Cockpit HMVSC",
+  },
+  {
+    id: 2,
+    description: "Cockpit Corpo Clínico",
+  },
+  {
+    id: 3,
+    description: "Cockpit Telemedicina",
+  },
+  {
+    id: 4,
+    description: "Portal Empresas",
+  },
+  {
+    id: 5,
+    description: "Cockpit Clínica Einstein",
+  },
+  {
+    id: 6,
+    description: "Cockpit Telereabilitação",
+  },
+  {
+    id: 7,
+    description: "Central de Requisições",
+  },
+  {
+    id: 8,
+    description: "Cockpit HStory",
+  }
+]
---
+++ prisma/seeders/post-classification.ts
@@ -0,0 +1,14 @@
+export const postClassification = [
+  {
+    id: 1,
+    description: 'Features'
+  },
+  {
+    id: 2,
+    description: 'Melhorias'
+  },
+  {
+    id: 3,
+    description: 'Ajustes'
+  }
+]
