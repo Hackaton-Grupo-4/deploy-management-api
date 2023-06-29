b83e5c8e2e368cb975ba66101f682d3134ef9d79
Joao Soares <joao.pssoares@einstein.br>
2023-06-29 13:37:02 -0300

    feat: add LogDecorator in HistoryController

--- src/main/factories/controllers/history-controller-factory.ts
+++ src/main/factories/controllers/history-controller-factory.ts
@@ -3,9 +3,11 @@ import { HistoryRepository } from "@/infra/db/prisma"
 import { PrismaHelper } from "@/infra/db/prisma/helpers/prisma-helper"
 import { HistoryController } from "@/presentation/controllers"
 import { Controller } from '@/presentation/protocols'
+import { makeLogControllerDecorator } from "../decorators/log-controller-decorator-factory"
 
 export const makeHistoryController = (): Controller => {
     const historyRepository = new HistoryRepository(PrismaHelper)
     const dbLoadHistory = new DbLoadHistory(historyRepository)
-    return new HistoryController(dbLoadHistory)
+    const historyController = new HistoryController(dbLoadHistory)
+    return makeLogControllerDecorator(historyController)
 }
