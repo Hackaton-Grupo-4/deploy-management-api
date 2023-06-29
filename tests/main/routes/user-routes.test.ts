f7a942450f535c5cb692722c46e7e3879a959620
Joao Soares <joao.pssoares@einstein.br>
2023-06-29 14:51:52 -0300

    fix: resolve user integration test

--- tests/main/routes/user-routes.test.ts
+++ tests/main/routes/user-routes.test.ts
@@ -2,20 +2,12 @@ import app from '@/main/config/app'
 import request from 'supertest'
 
 describe('User Routes', () => {
-  test('Should return 403 on add user without accessToken', async () => {
-    await request(app)
-      .post('/api/user')
-      .send({
-        name: 'João Soares',
-      })
-      .expect(403)
-  })
-
   test('Should return 200 on add user with valid accessToken', async () => {
     await request(app)
       .post('/api/user')
       .send({
         name: 'João Soares',
+        role: 'Telemedicina'
       })
       .expect(200)
   })
